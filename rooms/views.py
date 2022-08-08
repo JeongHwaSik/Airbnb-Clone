2b63139eeb9c3db567510e6dfda52baf499418be
JeongHwaSik <99hwasikk@gmail.com>
2022-08-08 15:31:57 +0900

    pagination using django objects
    making pagination using built-in django objects

--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,17 @@
+from math import ceil
 from django.shortcuts import render
+from django.core.paginator import Paginator
+from . import models
 
 
 def all_rooms(request):
-    return render(request, "all_rooms.html")
+    page = request.GET.get("page")
+    room_list = models.Room.objects.all()
+    paginator = Paginator(room_list, 10)
+    rooms = paginator.get_page(page)
+    print(vars(rooms.paginator))
+    return render(
+        request,
+        "rooms/home.html",
+        context={"rooms": rooms},
+    )
