2a68c26c55a9930e8676e535188799ef5c454cac
JeongHwaSik <99hwasikk@gmail.com>
2022-08-10 16:50:00 +0900

    finishing the searching form
    finishing the searching form using <form>

--- rooms/views.py
+++ rooms/views.py
@@ -1,4 +1,8 @@
+from cgi import print_exception
+from mimetypes import guess_type
 from django.views.generic import ListView, DetailView
+from django.shortcuts import render
+from django_countries import countries
 from . import models
 
 
@@ -19,3 +23,48 @@ class RoomDetail(DetailView):
 
     model = models.Room
     # pk_url_kwarg = "pk"
+
+
+def search(request):
+    city = request.GET.get("city", default="Anywhere")
+    city = city.capitalize()
+    country = request.GET.get("country", default="KR")
+    room_type = int(request.GET.get("room_type", default=0))
+    price = int(request.GET.get("price", 0))
+    guests = int(request.GET.get("guests", 0))
+    beds = int(request.GET.get("beds", 0))
+    bedrooms = int(request.GET.get("bedrooms", 0))
+    baths = int(request.GET.get("baths", 0))
+    instant = request.GET.get("instant", False)
+    super_host = request.GET.get("super_host", False)
+    s_amenities = request.GET.getlist("amenities")
+    s_facilities = request.GET.getlist("facilities")
+    print(instant, super_host)
+
+    form = {
+        "city": city,
+        "selected_room_type": room_type,
+        "selected_country": country,
+        "price": price,
+        "guests": guests,
+        "beds": beds,
+        "bedrooms": bedrooms,
+        "baths": baths,
+        "instant": instant,
+        "super_host": super_host,
+        "selected_amenities": s_amenities,
+        "selected_facilities": s_facilities,
+    }
+
+    room_types = models.RoomType.objects.all()
+    amenities = models.Amenity.objects.all()
+    facilities = models.Facility.objects.all()
+
+    choices = {
+        "countries": countries,
+        "room_types": room_types,
+        "amenities": amenities,
+        "facilities": facilities,
+    }
+
+    return render(request, "rooms/search.html", context={**form, **choices})
