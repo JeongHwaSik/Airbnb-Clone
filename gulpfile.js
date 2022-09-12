2c2f5a0757300d980aad485318b06fa33189e6bb
JeongHwaSik <99hwasikk@gmail.com>
2022-09-12 16:51:10 +0900

    using tailwind
    after setting up use tailwind css

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
-const gulp = require("gulp")
+const gulp = require("gulp");
 
 const css = () => {
     const postCSS = require("gulp-postcss");
-    const sass = require("gulp-sass")(require("sass"));
+    var sass = require("gulp-sass")(require('sass'));
     const minify = require("gulp-csso");
     sass.compiler = require("node-sass");
     return gulp
-        .src("assets/scss/styles.scss", { allowEmpty: true })
+        .src("assets/scss/styles.scss")
         .pipe(sass().on("error", sass.logError))
         .pipe(postCSS([require("tailwindcss"), require("autoprefixer")]))
         .pipe(minify())
